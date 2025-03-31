b855a0974dfa5ee8d0842bebd9ade704008602cb
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-31 09:16:12 -0300

    DesafioComplementar

---
+++ script.js
@@ -0,0 +1,15 @@
+let nome = prompt('Digite seu nome');
+let sobrenome = prompt('Digite seu sobrenome');
+console.log('O usuário ' + nome + ' ' + sobrenome + ' logou no site');
+alert('Olá ' + nome + ' ' + sobrenome + '!' );
+
+
+let anoAtual = 2025;
+let idadeAtual = prompt('Insira sua idade do final desse ano:');
+let idadeAgora = parseInt(idadeAtual);
+let anoNascimento = anoAtual - idadeAgora;
+console.log('O ' + nome + ' nasceu em ' + anoNascimento);
+alert('Você nasceu em ' + anoNascimento + '!');
+
+
+
